9e040c2bc847694368b8f1258f6bab7b3ecf951e
mariusbayizere <bayizeremarius119@gmail.com>
2024-08-25 11:25:22 +0200

    The first commit of the First Task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,12 +22,23 @@ class TestAccessNestedMap(unittest.TestCase):
                                expected: any) -> None:
         """
         Test that `access_nested_map` returns the expected output.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map: dict,
+                                         path: tuple) -> None:
+        """
+        Test that `access_nested_map` raises a KeyError for invalid paths.
 
         :param nested_map: The nested map (dictionary) to access.
         :param path: The path (tuple) to navigate within the nested map.
-        :param expected: The expected value from the nested map.
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
 
 
 if __name__ == "__main__":
