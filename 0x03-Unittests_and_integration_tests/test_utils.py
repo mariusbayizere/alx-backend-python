776b3f141c666510ccece7dcc1a65261f37f990e
mariusbayizere <bayizeremarius119@gmail.com>
2024-08-25 10:59:41 +0200

    The first commit of the First Task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,6 +14,22 @@ class TestAccessNestedMap(unittest.TestCase):
     """
 
     @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map: dict, path: tuple,
+                               expected: any) -> None:
+        """
+        Test that `access_nested_map` returns the expected output.
+
+        :param nested_map: The nested map (dictionary) to access.
+        :param path: The path (tuple) to navigate within the nested map.
+        :param expected: The expected value from the nested map.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
     ])
