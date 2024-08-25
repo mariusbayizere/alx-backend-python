8ac71b20f32a775cf52f7f9a3dc650264c443b9f
mariusbayizere <bayizeremarius119@gmail.com>
2024-08-25 12:34:00 +0200

    push task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,9 +3,10 @@
 Unit tests for the GithubOrgClient class from the client module.
 """
 
+from typing import Dict, Any
 from parameterized import parameterized
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from client import GithubOrgClient
 
 
@@ -19,9 +20,15 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc", {"org": "abc"})
     ])
     @patch('client.get_json', return_value={"org": "google"})
-    def test_org(self, org_name, expected_result, mock_get_json):
+    def test_org(self, org_name: str, expected_result: Dict[str, Any],
+                 mock_get_json: Any) -> None:
         """
         Test that GithubOrgClient.org returns the correct value.
+
+        Args:
+            org_name (str): The name of the organization.
+            expected_result (Dict[str, Any]): The expected result.
+            mock_get_json (Any): The mock of the get_json function.
         """
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org(), expected_result)
