95ba4949e3ebd7908aebc7e0184d47444cb1f328
mariusbayizere <bayizeremarius119@gmail.com>
2024-08-26 07:29:57 +0200

    testing pushing pagination

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient  # Ensure this module is present
 
@@ -22,6 +22,25 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(client.org(), expected_response)
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
+        )
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """
+        Test that _public_repos_url returns the expected value
+        when org is mocked.
+        """
+        # Mocked payload for the org property
+        mock_org.return_value = {
+            'repos_url': 'https://api.github.com/orgs/google/repos'
+        }
+
+        client = GithubOrgClient('google')
+
+        # Test that _public_repos_url returns the expected repos_url
+        self.assertEqual(
+            client._public_repos_url,
+            'https://api.github.com/orgs/google/repos'
         )
 
 
